8377f6dd13bb15cc90c2815ec319a4fa4d149168
ekazakaria02 <ekaputrazakaria123@gmail.com>
2024-07-16 14:33:30 +0800

    aplod tugas

---
+++ lib/example_variabel.dart
@@ -0,0 +1,15 @@
+void main() {
+  //mendeklarasikan variabel
+  String nama = 'Eka Zakaria';
+  String asal = 'Gorontalo';
+  num usia = 20; //digunakan untuk menyimpan segala jenis nomor
+  num tinggi = 1.75;
+  bool isMarried = false;
+
+  //mencetak nilai variabel
+  print("Name is $nama");
+  print("Address is $asal");
+  print("Age is $usia");
+  print("Height is $tinggi");
+  print("status menikah adalah $isMarried");
+}
